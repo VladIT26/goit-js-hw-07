d4ebe49d68439728f9e1e8076bdaf2a33a571e9c
VladIT26 <vlad.ua26@gmail.com>
2022-06-03 11:07:23 +0300

    go

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
 
-const itemsOnList = document.querySelector('.gallery')
+const itemsOnList = document.querySelector('div.gallery')
 const makeListMarkupOfImages = ({original, preview, description}) => {
     return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -21,26 +21,11 @@ itemsOnList.insertAdjacentHTML('afterbegin', makeImages)
 
 itemsOnList.addEventListener('click', modalShow);
 
-let modalWindow;
-
 function modalShow(event) {
     event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-    modalWindow = basicLightbox.create(
-        `<img src="${event.target.dataset.source}" width="800" height="600">`,
-        { onShow: () => window.addEventListener('keydown', closeModalOnKey),
-        onClose: () => window.removeEventListener('keydown', closeModalOnKey)}),
+    const modalWindow = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="800" height="600">`)
     modalWindow.show();
 };
 
-
-
-
-function closeModalOnKey(event) {
-  if (event.code === 'Escape') {
-    modalWindow.close();
-  }
-}
 
